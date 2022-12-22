b1ace66c1c5762059cab54d8f3ff6406f6eb34a6
ReaperMaga <reapermaga@gmail.com>
2022-12-23 00:45:00 +0100

    Added discord id

--- src/main/java/in/prismar/game/bundle/BundleFacade.java
+++ src/main/java/in/prismar/game/bundle/BundleFacade.java
@@ -1,10 +1,12 @@
 package in.prismar.game.bundle;
 
+import in.prismar.api.bundle.BundleProvider;
 import in.prismar.game.Game;
 import in.prismar.game.bundle.repository.BundleRepository;
 import in.prismar.game.bundle.repository.FileBundleRepository;
 import in.prismar.library.meta.anno.Service;
 import lombok.Getter;
+import org.bukkit.entity.Player;
 
 /**
  * Copyright (c) Maga, All Rights Reserved
@@ -14,11 +16,16 @@ import lombok.Getter;
  **/
 @Service
 @Getter
-public class BundleFacade {
+public class BundleFacade implements BundleProvider {
 
     private final BundleRepository repository;
 
     public BundleFacade(Game game) {
         this.repository = new FileBundleRepository(game.getDefaultDirectory());
+    }
+
+    @Override
+    public void give(Player player, String bundle) {
+
     }
 }
