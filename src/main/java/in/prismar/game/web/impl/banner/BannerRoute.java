8a47f4f22466849d0d1feaf1a022f6b72a339d6e
ReaperMaga <reapermaga@gmail.com>
2023-05-18 14:45:18 +0200

    Added clan live update

--- src/main/java/in/prismar/game/web/impl/banner/BannerRoute.java
+++ src/main/java/in/prismar/game/web/impl/banner/BannerRoute.java
@@ -1,27 +1,14 @@
 package in.prismar.game.web.impl.banner;
 
-import in.prismar.api.PrismarinConstants;
+import in.prismar.api.PrismarinApi;
+import in.prismar.api.clan.Clan;
+import in.prismar.api.clan.ClanProvider;
 import in.prismar.game.Game;
-import in.prismar.game.extraction.ExtractionFacade;
-import in.prismar.game.ffa.GameMapFacade;
 import in.prismar.game.web.WebServer;
-import in.prismar.game.web.impl.banner.request.Banner;
-import in.prismar.game.web.impl.banner.request.BannerPattern;
+import in.prismar.game.web.impl.banner.request.BannerRequest;
 import in.prismar.game.web.impl.banner.response.BannerResponse;
-import in.prismar.game.web.impl.response.PlayerResponse;
-import in.prismar.game.web.route.GetWebRoute;
 import in.prismar.game.web.route.PostWebRoute;
 import io.javalin.http.Context;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.block.banner.Pattern;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BannerMeta;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 /**
  * Copyright (c) Maga, All Rights Reserved
@@ -35,32 +22,15 @@ public class BannerRoute extends PostWebRoute<BannerResponse> {
     private final WebServer server;
 
     public BannerRoute(Game game, WebServer server) {
-        super("banner/{player}");
+        super("banner");
         this.game = game;
         this.server = server;
     }
 
     @Override
     public BannerResponse onRoute(Context context) {
-        final String id = context.pathParam("player");
-        Player player = Bukkit.getPlayer(id);
-        if(player == null) {
-            return new BannerResponse(false);
-        }
-        Banner banner = server.getGson().fromJson(context.body(), Banner.class);
-        Bukkit.getScheduler().runTask(game, () -> {
-            ItemStack stack = new ItemStack(Material.valueOf(banner.getBaseColor().name() + "_BANNER"));
-            BannerMeta meta = (BannerMeta) stack.getItemMeta();
-            List<Pattern> patterns = new ArrayList<>();
-            for(BannerPattern pattern : banner.getPatterns()) {
-                patterns.add(new Pattern(pattern.getColor(), pattern.getPattern()));
-            }
-            meta.setPatterns(patterns);
-            stack.setItemMeta(meta);
-
-            player.getInventory().addItem(stack);
-            player.sendMessage(PrismarinConstants.PREFIX + "§7You have received a §abanner");
-        });
-        return new BannerResponse(true);
+        BannerRequest banner = server.getGson().fromJson(context.body(), BannerRequest.class);
+        ClanProvider<Clan> clanProvider = PrismarinApi.getProvider(ClanProvider.class);
+        return new BannerResponse(clanProvider.refreshBanner(banner.getId()));
     }
 }
--- src/main/java/in/prismar/game/web/impl/banner/request/BannerRequest.java
+++ src/main/java/in/prismar/game/web/impl/banner/request/BannerRequest.java
@@ -1,11 +1,7 @@
 package in.prismar.game.web.impl.banner.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.bukkit.DyeColor;
-import org.bukkit.block.banner.PatternType;
 
 /**
  * Copyright (c) Maga, All Rights Reserved
@@ -13,10 +9,9 @@ import org.bukkit.block.banner.PatternType;
  * Proprietary and confidential
  * Written by Maga
  **/
-@NoArgsConstructor
-@Getter @Setter
-public class BannerPattern {
+@Getter
+@Setter
+public class BannerRequest {
 
-    private DyeColor color;
-    private PatternType pattern;
+    private String id;
 }
