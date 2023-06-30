b849fdfdd1e8dac94df5366ef729a003c6bfb8d6
ReaperMaga <reapermaga@gmail.com>
2023-07-01 01:45:46 +0200

    Added battlepass exp

--- src/main/java/in/prismar/game/warzone/listener/MobDeathListener.java
+++ src/main/java/in/prismar/game/warzone/listener/MobDeathListener.java
@@ -2,6 +2,7 @@ package in.prismar.game.warzone.listener;
 
 import in.prismar.api.PrismarinApi;
 import in.prismar.api.PrismarinConstants;
+import in.prismar.api.battlepass.BattlePassProvider;
 import in.prismar.api.configuration.ConfigStore;
 import in.prismar.api.user.User;
 import in.prismar.api.user.UserProvider;
@@ -32,6 +33,7 @@ public class MobDeathListener implements Listener {
 
     private final ConfigStore configStore;
     private final UserProvider<User> userProvider;
+    private BattlePassProvider battlePassProvider;
 
     public MobDeathListener() {
         this.configStore = PrismarinApi.getProvider(ConfigStore.class);
@@ -43,16 +45,28 @@ public class MobDeathListener implements Listener {
         if(event.getKiller() instanceof Player player) {
             final String name = event.getMob().getType().getInternalName();
             final String moneyRaw = configStore.getProperty("warzone.mobs." + name.toLowerCase() + ".reward");
+            final String battlePassRaw = configStore.getProperty("warzone.mobs." + name.toLowerCase() + ".battlepass");
             if(moneyRaw != null) {
                 final String[] split = moneyRaw.split("-");
+                final String[] splitBattlePass = battlePassRaw.split("-");
                 double money = MathUtil.randomDouble(Double.parseDouble(split[0]), Double.parseDouble(split[1]));
+                int battlepass = MathUtil.random(Integer.parseInt(splitBattlePass[0]), Integer.parseInt(splitBattlePass[1]));
                 User user = userProvider.getUserByUUID(player.getUniqueId());
                 user.getSeasonData().setBalance(user.getSeasonData().getBalance() + money);
+                getBattlePassProvider().addExp(user, battlepass);
                 player.sendMessage(PrismarinConstants.PREFIX + "§7You received §6" + NumberFormatter.formatDoubleToThousands(money) + "$ §7for killing a §c" + event.getMob().getDisplayName());
+                player.sendMessage(PrismarinConstants.PREFIX + "§7You received §a" + NumberFormatter.formatDoubleToThousands(money) + " Battlepass EXP §7for killing a §c" + event.getMob().getDisplayName());
+
                 userProvider.saveAsync(user, true);
                 warzoneService.getClanStatsProvider().addMobKills(player);
             }
         }
+    }
 
+    public BattlePassProvider getBattlePassProvider() {
+        if(battlePassProvider == null) {
+            battlePassProvider = PrismarinApi.getProvider(BattlePassProvider.class);
+        }
+        return battlePassProvider;
     }
 }
